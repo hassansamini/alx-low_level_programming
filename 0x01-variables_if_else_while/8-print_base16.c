61f900dbcf7d7dea7e0334bede0454bb1d0fdc32
hassansamini <samini099@gmail.com>
2023-08-11 16:49:30 -0700

    add new feature to convert print base 16 in lower case

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,11 +4,11 @@
 
 /**
  * main - Prints all numbers of base 16 in lowercase.
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {	char digit;
+
 	for (digit = '0'; digit <= '9'; digit++)
 	{	putchar(digit); }
 
