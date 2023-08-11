44efbde00b9b99bafc2c911ddbd45ff7c398e902
hassansamini <samini099@gmail.com>
2023-08-11 01:36:25 -0700

    add puts function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*Task 2: printingstring output using printf instead of puts*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
