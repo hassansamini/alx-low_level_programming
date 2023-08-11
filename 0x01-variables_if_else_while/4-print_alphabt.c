a4ef2ba5768ba330911778da3567b0b5bd2302cd
hassansamini <samini099@gmail.com>
2023-08-11 14:10:12 -0700

    add lower case functin]onality

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Print negative or positive
+ * Print lower case letter a-z except q/e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	n = 'a';
+
+	while (n <= 'z')
+	{
+		if (n !='e' | n!='q')
+		{
+		putchar(n);
+		n++;
+		}
+		else
+		{
+		pass
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
