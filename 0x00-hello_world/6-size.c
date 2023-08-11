24ed90501944534461ec6e9435d1260bef2860a7
hassansamini <samini099@gmail.com>
2023-08-11 02:15:35 -0700

    add sizeof()function

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,11 @@ int main(void)
     int b;
     long int c;
     long long int d;
-    float d;
-    printf("Size of char: %lu byte(s)\n", sizeof(a));
-    printf("Size of int: %lu byte(s)\n", sizeof(b));
-    printf("Size of long int: %lu byte(s)\n", sizeof(c));
-    printf("Size of long long int: %lu byte(s)\n", sizeof(d));
-    printf("Size of float: %lu byte(s)\n", sizeof(d));
+    float e;
+    printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
+    printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
+    printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+    printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+    printf("Size of float: %lu byte(s)\n",(unsigned long) sizeof(e));
     return (0);
 }
