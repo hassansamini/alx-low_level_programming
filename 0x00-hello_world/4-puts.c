6b900701ee80b39f37fe3b825b874013907bc181
hassansamini <samini099@gmail.com>
2023-08-11 01:17:56 -0700

    add puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 #include <stdio.h>
 /*
 *Program prints a text using puts function
 */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle,\n");
+    puts("\"Programming is like building a multilingual puzzle\n");
     return 0;
 }
