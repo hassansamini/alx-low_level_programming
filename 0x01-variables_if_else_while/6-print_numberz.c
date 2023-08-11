a630792a148f6558eec95af6e46457ea215e41a8
hassansamini <samini099@gmail.com>
2023-08-11 15:15:54 -0700

    adding new feature and printing using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Print negative or positive
+ * Print lower case letter
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	int i = 0;
+	int n = 9;
+
+	while (i <= n)
+	{
+	putchar("%d", i);
+	i++;
+	}
+	putchar("\n");
+	return (0);
+}
