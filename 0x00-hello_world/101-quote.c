e2f68cdad21304084d763947d6e582c8817b6cb8
hassansamini <samini099@gmail.com>
2023-08-11 04:37:05 -0700

    add Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+/**
+ * main - Quote
+ * Use of printf
+ * Return: Always 0 (Success)
+ */
+#include <unistd.h>
+int main(void)
+{
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
+}
+
