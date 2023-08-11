41b71a076082a09b4d89b62958a8e32157e6cca5
hassansamini <samini099@gmail.com>
2023-08-11 13:42:30 -0700

    add lower case functin]onality

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Print negative or positive
+ * Print lower case letter
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	n = 'a';
+
+	while (n <= 'z')
+	{
+	putchar(n);
+	putchar(n+32);
+	n++;
+	}
+
+	putchar('\n');
+	return (0);
+}
