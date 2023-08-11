529c4af095db403fda4222dd3a73bba326134960
hassansamini <samini099@gmail.com>
2023-08-11 15:03:11 -0700

    adding new feature

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Print negative or positive
+ * Print lower case letter
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int i=0;
+	n = 9;
+
+	while (i <= n)
+	{
+	printf("%d\n",n);
+	n++;
+	}
+
+	return (0);
+}
