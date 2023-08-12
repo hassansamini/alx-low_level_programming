38ba751bd6c82b4ee3ceeba674917aa268869373
hassansamini <samini099@gmail.com>
2023-08-11 17:15:00 -0700

    add files

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,7 +10,7 @@ int main(void)
 {	int digit;
 
 	for (digit = '0'; digit <= '9'; digit++)
-	{	putchar(digit); }
+	{	putchar(digit + '0'); }
 	if(digit != 9){putchar(','); putchar('');}}
 putchar('\n');
 return (0);
