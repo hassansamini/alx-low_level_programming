9e1fe64a09b3647467c6eed1734ebe105cb7a529
hassansamini <samini099@gmail.com>
2023-08-11 17:37:04 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,8 +10,9 @@ int main(void)
 {	int digit;
 
 	for (digit = '0'; digit <= '9'; digit++)
-	{	putchar(digit); 
-	if(digit != 9){putchar(','); putchar(' ');}}
+	{	putchar(digit)	
+		if(digit != '9'){putchar(','); putchar(' '); }
+	}
 putchar('\n');
 return (0);
 }
