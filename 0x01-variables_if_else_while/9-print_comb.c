68b78a880ea356602e172e3bc6e0b47e86e82b84
hassansamini <samini099@gmail.com>
2023-08-11 17:05:53 -0700

    add file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints all numbers of base 16 in lowercase.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{	putchar(digit); }
+	if(digit != 9){putchar(','),putchar('')}}
+putchar('\n');
+return (0);
+}
