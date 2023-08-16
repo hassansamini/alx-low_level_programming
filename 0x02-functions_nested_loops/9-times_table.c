5eec393eedfc30ff69d20ebc70a8437fbf8e4d85
hassansamini <samini099@gmail.com>
2023-08-16 14:03:34 -0700

    addtimetabbl

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table
+ */
+void times_table(void)
+{
+	int x, y, z, u, d;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+			u = z % 10;
+			d = (z - u) / 10;
+			if (y != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (z >= 10)
+			{
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(z + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
