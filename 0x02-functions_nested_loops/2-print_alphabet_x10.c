ce49d1371b0f1bfbcb35079c752409600cd8654d
hassansamini <samini099@gmail.com>
2023-08-16 06:31:32 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,17 @@
  */
 void print_alphabet_x10(void)
 {
-    char ch;
-    int i = 0;
+	char ch;
+	int i = 1;
 
-    while (i <= 9)
-    {
-        for (ch = 'a'; ch <= 'z'; ch++)
-        {
-            _putchar(ch);
-        }
-        _putchar('\n');
-        ++i;
-    }
+	while (i <= 10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+		++i;
+	}
 }
 
