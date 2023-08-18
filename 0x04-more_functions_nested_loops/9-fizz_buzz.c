520b344b1d38ba0a3e36693d539c1bdb80a998c7
hassansamini <samini099@gmail.com>
2023-08-18 12:23:04 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * fizz_buzz - Implements the FizzBuzz problem for numbers 1 to 100
+ *
+ * Return: void
+ */
+void fizz_buzz(void)
+{
+    int i;
+
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("FizzBuzz");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("Fizz");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("Buzz");
+        }
+        else
+        {
+            printf("%d", i);
+        }
+
+        if (i < 100)
+        {
+            printf(" ");
+        }
+        else
+        {
+            printf("\n");
+        }
+    }
+}
+
+int main(void)
+{
+    fizz_buzz();
+    return 0;
+}
